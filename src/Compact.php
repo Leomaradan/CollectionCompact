9870e5d489b9c19ab4d42ba6a38760bc61ea7d38
Léo Maradan <3043485+Leomaradan@users.noreply.github.com>
2019-02-22 09:21:09 +0100

    Update Compact.php

--- src/Compact.php
+++ src/Compact.php
@@ -54,20 +54,20 @@ class Compact {
 		return $string;
 	}
 
-	public function unpack($packedString) {
+	public static function unpack($packedString) {
 
 		if(!is_string($packedString)) {
 			throw new \Exception("$packedString is not a string", 1);
 		}
 
-		$volumes = $packedString.split(',');
+		$volumes = explode(',', $packedString);
 		$volumes_list = [];
 
 		foreach ($volumes as $volume) {
 			if(is_numeric($volume)) {
 				$volumes_list[] = $volume;
 			} else {
-				$group = $volume.split('-');
+				$group = explode('-', $volume);
 				$start = $group[0];
 				$end = $group[1];
 				for ($i=$start; $i <= $end ; $i++) {
